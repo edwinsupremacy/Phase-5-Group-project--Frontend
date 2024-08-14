5e02689f68dc4051ff38458a4b059d930dc9781c
s-k3nyyy <collo.c.heg@gmail.com>
2024-08-14 12:21:01 +0300

    collo

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
+  base: '/Phase-5-Group-project--Frontend/', // This should match your repository name
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
     },
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
   },
 });
